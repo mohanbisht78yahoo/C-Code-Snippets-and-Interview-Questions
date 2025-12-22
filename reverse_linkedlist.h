88404997b46d550d58c9f0d38f41c8ea2868b6fb
Mohan Singh Bisht <95560724+mohanbisht78yahoo@users.noreply.github.com>
2025-12-22 16:11:59 +0530

    Update reverse_linkedlist.h

--- reverse_linkedlist.h
+++ reverse_linkedlist.h
@@ -1,3 +1,5 @@
+/* Reversing a linked list involves changing direction of the next pointers 
+so that the last node becomes the new head and the original head becomes the new tail */ 
 template<typename T>
 NodePtr<T>* reverse(NodePtr<T>*head)
 {
