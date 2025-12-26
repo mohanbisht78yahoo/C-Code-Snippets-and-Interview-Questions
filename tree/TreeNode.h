b857cd3f976558ce7811a2fb79e6ffe888dc2c37
Mohan Singh Bisht <95560724+mohanbisht78yahoo@users.noreply.github.com>
2025-12-27 02:11:16 +0530

    Update TreeNode.h

--- tree/TreeNode.h
+++ tree/TreeNode.h
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-//SortedInsertedLinkedList
 template <typename T>
 struct NODE {
    NODE* left = nullptr;
@@ -19,3 +18,31 @@ void print_inorder(NodePtr<T> root)
     cout<< root->data << " ";
     print_inorder(root->right);
 }
+
+template <typename T>
+int height(NodePtr<T> root) {
+    if(root == nullptr) return 0;
+    return 1 + max(height(root->left), height(root->right));
+}
+
+template <typename T>
+bool is_subtree(NodePtr<T> root1, NodePtr<T> root2) {
+    
+    if(root1 == nullptr && root2 == nullptr) return true;    
+    if( is_sametree(root1, root2)) return true;
+    if( is_subtree(root1->left, root2)) return true;
+    if( is_subtree(root1->right, root2)) return true;
+    return false;
+}
+
+template <typename T>
+bool is_sametree(NodePtr<T> root1, NodePtr<T> root2) 
+{
+    if(root1 == nullptr && root2 == nullptr) return true;
+    if(root1 == nullptr || root2 == nullptr) return false;
+    if(root1->data != root2->data) return false;
+    
+    return is_sametree(root1->left, root2->left) &&
+        is_sametree(root1->right, root2->right);
+}
+
