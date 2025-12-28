74648446a71098e2bedd722e0701e6dd6c92b74d
Mohan Singh Bisht <95560724+mohanbisht78yahoo@users.noreply.github.com>
2025-12-29 02:56:16 +0530

    Update circularlinkedlist.h

--- linkedlist/circularlinkedlist.h
+++ linkedlist/circularlinkedlist.h
@@ -1,58 +1,219 @@
 #include <iostream>
+#include <limits.h>
+#include <list>
+
 using namespace std;
 
 template <typename T>
 struct NODE
 {
-  NODE<T>* next;
-  T data;
-  NODE(T val) { data = val;}
+  NODE<T>* next = nullptr;
+  std::list<T> dataList;
+  //T data;
+
+  NODE(T val) 
+  { 
+    //data = val;
+    dataList.push_back(val);
+  }
+
+  friend std::ostream& operator<< (std::ostream& stream, const NODE<T>& node);
+
+  void print()
+  {
+    if(dataList.size() > 1) 
+    {
+        cout << "[ ";
+        for(auto itr = dataList.begin(); itr != dataList.end(); itr++)
+            cout << *itr << ", ";
+        cout << "]";
+    }
+    else cout << " " << dataList.front() << ", ";
+  }
+
+  ~NODE() { cout << endl << "destroying: " << dataList.front();}
 };
+
+template <typename T>
+std::ostream& operator<<(std::ostream& stream,
+                     const NODE<T>& node) {
+    node.print(stream); // Assuming you define print for matrix
+    return stream;
+ }
 
 template <typename T>
 using NodePtr = NODE<T>*;
 
 template <typename T>
-NodePtr<T> insert(NodePtr<T> head, T data)
+using ConstNodePtr = const NODE<T>*;
+
+template <
+    typename T,
+    unsigned int Capacity = UINT_MAX >
+class CircularList 
 {
-    NodePtr<T> newNode = new NODE<T>(data);
-    if(head == nullptr) {
-        head = newNode;
-        head->next = head;
-        return head;
+    static_assert(Capacity >= 1, "Capacity must be >= 1");
+
+private:
+    NodePtr<T> head = nullptr;
+
+public: 
+    class Iterator
+    {
+
+    private:
+        ConstNodePtr<T> currentNode;
+        ConstNodePtr<T> head;
+        bool started;
+
+    public:
+
+        Iterator(ConstNodePtr<T> p, ConstNodePtr<T> h, bool s = false)
+            : currentNode(p), head(h), started(s) {}
+
+        Iterator& operator=(NodePtr<T> pNode)
+        {
+            this->currentNode = pNode;
+            return *this;
+        }
+
+        /* prefix overlaod */
+        Iterator& operator++()
+        {
+            if (!currentNode) return *this;
+                currentNode = currentNode->next;
+
+            /* if looping started, and current node is at head
+            set it to nullptr to break the loop */
+            if (currentNode == head && started)
+                currentNode = nullptr;     // mark end when loop completed
+            started = true;
+            return *this;
+        }
+
+        // Postfix ++ overload
+        Iterator& operator++(int)
+        {
+            // reuse prefix logic
+            return ++(*this);
+        }
+
+        bool operator!=(const Iterator& iterator)
+        {
+            return currentNode != iterator.currentNode;
+        }
+        
+        T operator*()
+        {
+            return currentNode->dataList.front();
+        }
+    };
+
+
+public:
+    ~CircularList()
+    {
+        clear();
     }
 
-    NodePtr<T> tmp = head;
-    while(tmp->next != head) {
-        tmp = tmp->next;
+    void clear() 
+    {
+        if(!head) return;
+        NodePtr<T> tmpHead = head;
+        NodePtr<T> deleteItr = nullptr;
+        do
+        {
+            deleteItr = tmpHead;
+            tmpHead = tmpHead->next;
+            delete deleteItr;
+        } while( tmpHead != head );
+        head = nullptr;
     }
-  
-    newNode->next = head;  
-    tmp->next = newNode;
-    return head;
-}
 
-template <typename T>
-void print_list(NodePtr<T> head)
-{
-  if(!head) return;
+     // Root of LinkedList wrapped in Iterator type
+    Iterator begin()
+    {
+        return Iterator(head, head);
+    }
 
-  NodePtr<T> tmp = head;
-  do 
-  {
-    cout << tmp->data << " ";
-    tmp = tmp->next;
-  } while(tmp != head);
-}
+    Iterator end()
+    {
+        return Iterator(nullptr, head);
+    }
+
+    //void 
+    void remove(T data)
+    {
+
+    }
+
+    void push_back(T data)
+    {
+        if(head == nullptr) {
+            head = new NODE<T>(data);
+            head->next = head;
+            return;
+        }
+
+        int count = 2;
+        NodePtr<T> tail = head;
+        while(tail->next != head) {
+            if(tail->dataList.front() == data) {
+                tail->dataList.push_back(data);
+                return;
+            }
+
+            tail = tail->next;
+            count++;
+        }
+
+        if(tail->dataList.front() == data) {
+            tail->dataList.push_back(data);
+            return;
+        }
+
+        if(count <= Capacity) {
+            NodePtr<T> newNode = new NODE<T>(data);
+            newNode->next = head;  
+            tail->next = newNode;
+        }
+        /* we have reached the capacity */
+        else {
+            ///head->dataL = data;
+            head->dataList.clear(); head->dataList.push_back(data);
+            head = head->next;
+        }
+    }
+
+    void print()
+    {
+        if(!head) return;
+        NodePtr<T> tmp = head;
+        do 
+        {
+            tmp->print();            
+            tmp = tmp->next;
+        } while(tmp != head);
+
+        cout << endl;
+    }
+};
+
 
 int main()
 {
-    NodePtr<int> head = nullptr;
+    CircularList<int, 5> circularList;
+    for(int i=1;i<10;i++) {
+        circularList.push_back((uint)i);
+        if(i%2) circularList.push_back((uint)i);
+    }
 
-    head = insert(head, 6);
-    head = insert(head, 8);
-    head = insert(head, 1);
-    print_list(head);
+    circularList.print();
+
+    for(auto itr = circularList.begin(); itr != circularList.end(); itr++)
+    {
+        cout << *itr << " ";
+    }
 
     return 0;
 }
