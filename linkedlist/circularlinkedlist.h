571da9fcceadc3ef027af812db103030c282f44a
Mohan Singh Bisht <95560724+mohanbisht78yahoo@users.noreply.github.com>
2025-12-28 14:38:56 +0530

    Update circularlinkedlist.h

--- linkedlist/circularlinkedlist.h
+++ linkedlist/circularlinkedlist.h
@@ -1,19 +1,43 @@
+#include <iostream>
+using namespace std;
+
 template <typename T>
-typedef struct NODE
+struct NODE
 {
-  NODE* next;
+  NODE<T>* next;
   T data;
-}Node;
+  NODE(T val) { data = val;}
+};
 
 template <typename T>
-using NodePtr = Node<T>*;
+using NodePtr = NODE<T>*;
 
 template <typename T>
-void print_list(NodePtr<T> * head)
+NodePtr<T> insert(NodePtr<T> head, T data)
+{
+    NodePtr<T> newNode = new NODE<T>(data);
+    if(head == nullptr) {
+        head = newNode;
+        head->next = head;
+        return head;
+    }
+
+    NodePtr<T> tmp = head;
+    while(tmp->next != head) {
+        tmp = tmp->next;
+    }
+  
+    newNode->next = head;  
+    tmp->next = newNode;
+    return head;
+}
+
+template <typename T>
+void print_list(NodePtr<T> head)
 {
   if(!head) return;
 
-  NodePtr<T>* tmp = head;
+  NodePtr<T> tmp = head;
   do 
   {
     cout << tmp->data << " ";
@@ -21,3 +45,14 @@ void print_list(NodePtr<T> * head)
   } while(tmp != head);
 }
 
+int main()
+{
+    NodePtr<int> head = nullptr;
+
+    head = insert(head, 6);
+    head = insert(head, 8);
+    head = insert(head, 1);
+    print_list(head);
+
+    return 0;
+}
